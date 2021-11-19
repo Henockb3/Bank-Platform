8800ec2a87bb471b04f791dd295cdc2b298b15da
henockb3 <henockb3@gmail.com>
2021-11-19 09:05:52 -0800

    Implemented Account Interface

--- src/main/java/bankplatform/dao/AccountDao.java
+++ src/main/java/bankplatform/dao/AccountDao.java
@@ -1,4 +1,13 @@
 package bankplatform.dao;
 
+import bankplatform.dto.Account;
+import bankplatform.dto.Transaction;
+import bankplatform.dto.User;
+
+import java.util.List;
+
 public interface AccountDao {
+    Account createAccount(User userId);
+    List<Transaction> listAllTransactions(Account accountNumber);
+    List<Transaction> getTransactionByDate(Transaction transactionDate);
 }
--- src/main/java/bankplatform/dao/AccountDaoImpl.java
+++ src/main/java/bankplatform/dao/AccountDaoImpl.java
@@ -1,4 +1,24 @@
 package bankplatform.dao;
 
-public class AccountDaoImpl {
+import bankplatform.dto.Account;
+import bankplatform.dto.Transaction;
+import bankplatform.dto.User;
+
+import java.util.List;
+
+public class AccountDaoImpl implements AccountDao{
+    @Override
+    public Account createAccount(User userId) {
+        return null;
+    }
+
+    @Override
+    public List<Transaction> listAllTransactions(Account accountNumber) {
+        return null;
+    }
+
+    @Override
+    public List<Transaction> getTransactionByDate(Transaction transactionDate) {
+        return null;
+    }
 }
