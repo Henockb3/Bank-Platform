9977d3d2ef8ad382d926da0079232e65046d0dc8
hatim <hatim.joubair@gmail.com>
2021-11-19 12:14:31 -0500

    add userDao

--- src/main/java/bankplatform/dao/UserDao.java
+++ src/main/java/bankplatform/dao/UserDao.java
@@ -1,4 +1,14 @@
 package bankplatform.dao;
 
+import bankplatform.dto.User;
+
+import java.util.List;
+
 public interface UserDao {
+    User createUser();
+    User getUserByEmail(String email);
+    User updateUser(User user);
+    List<User> getUserList();
+    User removeUser();
+
 }
--- src/main/java/bankplatform/dao/UserDaoImpl.java
+++ src/main/java/bankplatform/dao/UserDaoImpl.java
@@ -1,4 +1,32 @@
 package bankplatform.dao;
 
-public class UserDaoImpl {
+import bankplatform.dto.User;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User createUser() {
+        return null;
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public User updateUser(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> getUserList() {
+        return null;
+    }
+
+    @Override
+    public User removeUser() {
+        return null;
+    }
 }
