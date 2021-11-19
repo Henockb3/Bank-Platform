40300636412115d2d42edfaf0367bc8631ffcca0
henockb3 <henockb3@gmail.com>
2021-11-19 12:08:07 -0800

    controller from nahom

--- src/main/java/bankplatform/controller/Controller.java
+++ src/main/java/bankplatform/controller/Controller.java
@@ -1,4 +1,24 @@
 package bankplatform.controller;
 
+import bankplatform.dto.Account;
+import bankplatform.dto.User;
+import bankplatform.service.BankServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class Controller {
+
+    @Autowired
+    BankServiceLayer serviceLayer;
+
+    @PostMapping("/begin")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createGame(User user, Account account){
+        serviceLayer.createAccount(user,account);
+    }
 }
