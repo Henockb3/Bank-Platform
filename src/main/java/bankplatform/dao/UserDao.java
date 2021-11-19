6e4ebcce9823bd19357259c6d858c3f1c3b1b531
hatim <hatim.joubair@gmail.com>
2021-11-19 12:33:23 -0500

    commit

--- src/main/java/bankplatform/dao/UserDao.java
+++ src/main/java/bankplatform/dao/UserDao.java
@@ -5,7 +5,7 @@ import bankplatform.dto.User;
 import java.util.List;
 
 public interface UserDao {
-    User createUser();
+    User createUser(User user);
     User getUserByEmail(String email);
     User updateUser(User user);
     List<User> getUserList();
--- src/main/java/bankplatform/dao/UserDaoImpl.java
+++ src/main/java/bankplatform/dao/UserDaoImpl.java
@@ -1,13 +1,23 @@
 package bankplatform.dao;
 
 import bankplatform.dto.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class UserDaoImpl implements UserDao {
+
+    JdbcTemplate jdbc;
+
     @Override
-    public User createUser() {
-        return null;
+    public User createUser(User user) {
+       final String INSERT_USER = "INSERT INTO user(userId,firstName,lastName,email,password)" +
+               "VALUES(?,?,?,?)";
+       jdbc.update(INSERT_USER,user.getFirstName(),user.getLastName(),user.getEmail(),user.getPassword());
+       int newId = jdbc.queryForObject("SELECT MAX(roundId) FROM round", Integer.class);
+
     }
 
     @Override
