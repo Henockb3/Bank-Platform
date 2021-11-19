4ca127b97e498dd950c3cba77dd625002ca5d6c1
javersa <javersa@gmu.edu>
2021-11-19 12:09:01 -0500

    Update TranscationDao.java

--- src/main/java/bankplatform/dao/TranscationDao.java
+++ src/main/java/bankplatform/dao/TranscationDao.java
@@ -1,4 +1,8 @@
 package bankplatform.dao;
 
 public interface TranscationDao {
+  void add(Transaction transaction);
+  List<Transaction> getAll();
+  List<Transaction> getByTimeRange(String beginning, String ending);
+  Transaction getByAccountNumber(int accountNumber);
 }
