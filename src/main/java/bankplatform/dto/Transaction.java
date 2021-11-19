aaf116cdcd6729aee2cc720e01fd2e0880c45c87
henockb3 <henockb3@gmail.com>
2021-11-19 08:51:28 -0800

    updated dto's

--- src/main/java/bankplatform/dto/Transaction.java
+++ src/main/java/bankplatform/dto/Transaction.java
@@ -1,4 +1,62 @@
 package bankplatform.dto;
 
+import java.math.BigDecimal;
+
 public class Transaction {
+    int transactionId;
+    int accountNumber;
+    String timeStamp;
+    BigDecimal transactionAmount;
+
+    public Transaction() {
+    }
+
+    public Transaction(int transactionId, int accountNumber, String timeStamp, BigDecimal transactionAmount) {
+        this.transactionId = transactionId;
+        this.accountNumber = accountNumber;
+        this.timeStamp = timeStamp;
+        this.transactionAmount = transactionAmount;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public BigDecimal getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(BigDecimal transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "transactionId=" + transactionId +
+                ", accountNumber=" + accountNumber +
+                ", timeStamp='" + timeStamp + '\'' +
+                ", transactionAmount=" + transactionAmount +
+                '}';
+    }
 }
